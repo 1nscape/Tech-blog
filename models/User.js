e1ce0b0e6cab6c71b6252113f7c0d30862d0c005
Max <max.belenchuk@gmail.com>
2023-04-14 15:26:58 -0300

    Criteria setup for id, user and password

--- models/User.js
+++ models/User.js
@@ -7,3 +7,27 @@ class User extends Model {
         return bcrypt.compareSync(loginPW, this.password)
     }
 }
+
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4]
+        }
+      }
+    },
+    
+);
