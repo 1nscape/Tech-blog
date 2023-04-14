be4e5c554f585d4467e59f2fb670e8952220c5b3
Max <max.belenchuk@gmail.com>
2023-04-14 15:10:09 -0300

    Method setup to check password

--- models/User.js
+++ models/User.js
@@ -2,3 +2,8 @@ const { Model, DataTypes } = require('sequlize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
+class User extends Model {
+    checkPassword(loginPW) {
+        return bcrypt.compareSync(loginPW, this.password)
+    }
+}
