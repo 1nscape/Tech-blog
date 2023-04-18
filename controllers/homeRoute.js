29f265265948efbd0e84840728fecac100d57560
Max <max.belenchuk@gmail.com>
2023-04-18 18:53:43 -0300

    Criteria defined for comments provided by user

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,2 +1,34 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
+
+
+router.get('/', (req, res)=> {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'post_text',
+            'created_at'
+        ],
+
+        include: [{
+            model: Comment,
+            attributes: [
+                'id',
+                'post_id',
+                'user_id',
+                'comment_text',
+                'created_at'
+            ],
+            include: {
+                model: User,
+                attributes: ['username']
+            
+            },
+
+            model: User,
+            attributes: ['username']
+
+        }]
+    })
+})
